a65f60ec540a3ed3817ef1545927001513e08eb6
小群的呦(❁´◡`❁) <littlechintw@gmail.com>
2019-09-12 15:25:14 +0800

    Create UVA10783_Odd Sum.c

---
+++ UVA10783_Odd Sum.c
@@ -0,0 +1,23 @@
+// 2019/9/12
+// Created by littlechin
+// Copyright littlechin. All rights reserved.
+
+#include<stdio.h>
+int main(void){
+	int n;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++){
+		int x,y;
+		int mix;
+		scanf("%d",&x);
+		scanf("%d",&y);
+		if(x%2==0){
+			x+=1;
+		}
+		if(y%2==0){
+			y-=1;
+		}
+		mix = (x+y)*((y-x)/2+1)/2;
+		printf("Case %d: %d\n",i+1,mix);
+	}
+}
