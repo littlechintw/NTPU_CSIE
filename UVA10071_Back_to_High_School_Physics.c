aba9c8d8a26d45b181e967b5adb4ddfa12ccc022
小群的呦(❁´◡`❁) <littlechintw@gmail.com>
2019-10-10 22:50:39 +0800

    Create UVA10071_Back_to_High_School_Physics.c

---
+++ UVA10071_Back_to_High_School_Physics.c
@@ -0,0 +1,11 @@
+// 2019/10/3
+// Created by littlechin
+// Copyright littlechin. All rights reserved.
+
+#include<stdio.h>
+int main(){
+	int v,t;
+	while(scanf("%d %d",&v,&t)!=EOF)
+		printf("%d\n",(2*v*t));
+	return 0;
+}
